8f3c700f6e470742a0a93bfc5f793f5c24178b48
Victor Lin <linvictorw@gmail.com>
2020-12-14 22:10:36 +0800

    Dark theme and shell of article

--- src/App.js
+++ src/App.js
@@ -1,24 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import { Typography, Grid } from '@material-ui/core';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Grid>
+      <Typography variant="h2">
+        BENJAMIN GRAHAM’S NET CURRENT ASSET VALUES
+      </Typography>
+        
+      <Typography variant="h3">
+        An Update to the Performance Update
+      </Typography>
+
+      <Typography variant="overline">
+        November 29, 2020
+      </Typography>
+
+      <Typography variant="h4">
+        Introduction
+      </Typography>
+
+      <Typography variant="body1">
+        Like trillions of people who came before me, I am trying to answer a question that will plague mankind for as long as equity markets still exist: Does Benjamin Graham’s net current asset strategy (or net net strategy) still work?
+      </Typography>
+    </Grid>
   );
 }
 
